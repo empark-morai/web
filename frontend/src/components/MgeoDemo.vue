4300ba3bb6cd4acee3c0c19b7feb709032b26526
DESKTOP-QR5QLHM\eunmi <empark@morai.ai>
2023-04-04 16:58:21 +0900

    test

--- frontend/src/components/MgeoDemo.vue
+++ frontend/src/components/MgeoDemo.vue
@@ -24,11 +24,11 @@ export default {
 
             var path = [ 	
                 new BABYLON.Vector3(-20, 0, 0),
-                new BABYLON.Vector3(-10, 0, 10),
+                new BABYLON.Vector3(-10, 10, 0),
                 new BABYLON.Vector3(0, 0, 0),
-                new BABYLON.Vector3(10, 0, 10),
+                new BABYLON.Vector3(10, 10, 0),
                 new BABYLON.Vector3(20, 0, 0),
-                new BABYLON.Vector3(10, 0, -10)
+                new BABYLON.Vector3(10, -10, 0)
             ]
             var line = this.create2DTest("line", {path: path, width:1.5}, this.scene);
         }
@@ -99,7 +99,6 @@ export default {
             let lines = BABYLON.MeshBuilder.CreateLines(link_info.idx, options, this.scene);
         },
         create2DTest(name, options, scene) {
-            
             //Arrays for vertex positions and indices
             var positions = [];
             var indices = [];
@@ -107,90 +106,70 @@ export default {
 
             var width = options.width / 2 || 0.5;
             var path = options.path;
-            var closed = options.closed || false;
-            /*if(options.standardUV === undefined) {
-                standardUV = true;
-            }
-            else {
-                standardUV = options.standardUV;
-            }*/
-        
-            var interiorIndex;
             
-            //Arrays to hold wall corner data 
-            var innerBaseCorners = [];
-            var outerBaseCorners = [];
-            
-            var outerData = [];
             var innerData = [];
+            var outerData = [];
             var angle = 0;
             
             var nbPoints = path.length;
             var line = BABYLON.Vector3.Zero();
             var nextLine = BABYLON.Vector3.Zero();
             path[1].subtractToRef(path[0], line);
+            
+            let lineNormal = new BABYLON.Vector3(-line.y, 1 * line.x, 0).normalize();
 
-            if(nbPoints > 2 && closed) {	
-                path[2].subtractToRef(path[1], nextLine);    
-                for(var p = 0; p < nbPoints; p++) {    
-                    angle = Math.PI - Math.acos(BABYLON.Vector3.Dot(line, nextLine)/(line.length() * nextLine.length()));            
-                    direction = BABYLON.Vector3.Cross(line, nextLine).normalize().y;                
-                    let lineNormal = new BABYLON.Vector3(-line.z, 0, 1 * line.x).normalize();
-                    line.normalize();
-                    innerData[(p + 1) % nbPoints] = path[(p + 1) % nbPoints].subtract(lineNormal.scale(width)).subtract(line.scale(direction * width/Math.tan(angle/2)));
-                    outerData[(p + 1) % nbPoints] = path[(p + 1) % nbPoints].add(lineNormal.scale(width)).add(line.scale(direction * width/Math.tan(angle/2)));        				
-                    line = nextLine.clone();        
-                    path[(p + 3) % nbPoints].subtractToRef(path[(p + 2) % nbPoints], nextLine);    
-                }
+            var sphere1 = BABYLON.MeshBuilder.CreateSphere("sphere1", {diameter: 8}, scene);
+            sphere1.position = new BABYLON.Vector3(lineNormal[0], lineNormal[1], lineNormal[2]);
+            const localAxes = new BABYLON.AxesViewer(this.scene, 10);
+            localAxes.xAxis.parent = sphere1;
+            localAxes.yAxis.parent = sphere1;
+            localAxes.zAxis.parent = sphere1;	
+
+            line.normalize()
+            innerData[0] = path[0].subtract(lineNormal.scale(width));
+            outerData[0] = path[0].add(lineNormal.scale(width));
+        
+            for(var p = 0; p < nbPoints - 2; p++) {	
+                path[p + 2].subtractToRef(path[p + 1], nextLine)
+                angle = Math.PI - Math.acos(BABYLON.Vector3.Dot(line, nextLine)/(line.length() * nextLine.length()))
+                let direction = BABYLON.Vector3.Cross(line, nextLine).normalize().z;			
+                lineNormal = new BABYLON.Vector3(-line.y, 1 * line.x, 0).normalize();
+                line.normalize();
+                innerData[p + 1] = path[p + 1].subtract(lineNormal.scale(width)).subtract(line.scale(direction * width/Math.tan(angle/2)));
+                outerData[p + 1] = path[p + 1].add(lineNormal.scale(width)).add(line.scale(direction * width/Math.tan(angle/2)));		
+                line = nextLine.clone();		
+            }
+            if(nbPoints > 2) {
+                path[nbPoints - 1].subtractToRef(path[nbPoints - 2], line);
+                lineNormal = new BABYLON.Vector3(-line.y, 1 * line.x, 0).normalize();
+                line.normalize();		
+                innerData[nbPoints - 1] = path[nbPoints - 1].subtract(lineNormal.scale(width));
+                outerData[nbPoints - 1] = path[nbPoints - 1].add(lineNormal.scale(width));
             }
             else {
-                let lineNormal = new BABYLON.Vector3(-line.z, 0, 1 * line.x).normalize();
-                line.normalize();		
-                innerData[0] = path[0].subtract(lineNormal.scale(width));
-                outerData[0] = path[0].add(lineNormal.scale(width));
-            
-                for(var p = 0; p < nbPoints - 2; p++) {	
-                    path[p + 2].subtractToRef(path[p + 1], nextLine);
-                    angle = Math.PI - Math.acos(BABYLON.Vector3.Dot(line, nextLine)/(line.length() * nextLine.length()));			
-                    let direction = BABYLON.Vector3.Cross(line, nextLine).normalize().y;			
-                    lineNormal = new BABYLON.Vector3(-line.z, 0, 1 * line.x).normalize();
-                    line.normalize();
-                    innerData[p + 1] = path[p + 1].subtract(lineNormal.scale(width)).subtract(line.scale(direction * width/Math.tan(angle/2)));
-                    outerData[p + 1] = path[p + 1].add(lineNormal.scale(width)).add(line.scale(direction * width/Math.tan(angle/2)));		
-                    line = nextLine.clone();			
-                }
-                if(nbPoints > 2) {
-                    path[nbPoints - 1].subtractToRef(path[nbPoints - 2], line);
-                    lineNormal = new BABYLON.Vector3(-line.z, 0, 1 * line.x).normalize();
-                    line.normalize();		
-                    innerData[nbPoints - 1] = path[nbPoints - 1].subtract(lineNormal.scale(width));
-                    outerData[nbPoints - 1] = path[nbPoints - 1].add(lineNormal.scale(width));
-                }
-                else{
-                    innerData[1] = path[1].subtract(lineNormal.scale(width));
-                    outerData[1] = path[1].add(lineNormal.scale(width));
-                }
-            }
+                innerData[1] = path[1].subtract(lineNormal.scale(width));
+                outerData[1] = path[1].add(lineNormal.scale(width));
+            } 
         
             var maxX = Number.MIN_VALUE;
             var minX = Number.MAX_VALUE;
-            var maxZ = Number.MIN_VALUE;
-            var minZ = Number.MAX_VALUE;
+            var maxY = Number.MIN_VALUE;
+            var minY = Number.MAX_VALUE;
             
             for(var p = 0; p < nbPoints; p++) {
                 positions.push(innerData[p].x, innerData[p].y, innerData[p].z);
                 maxX = Math.max(innerData[p].x, maxX);
                 minX = Math.min(innerData[p].x, minX);
-                maxZ = Math.max(innerData[p].z, maxZ);
-                minZ = Math.min(innerData[p].z, minZ);
+                maxY = Math.max(innerData[p].y, maxY);
+                minY = Math.min(innerData[p].y, minY);
             }
 
             for(var p = 0; p < nbPoints; p++) {
                 positions.push(outerData[p].x, outerData[p].y, outerData[p].z);
                 maxX = Math.max(innerData[p].x, maxX);
                 minX = Math.min(innerData[p].x, minX);
-                maxZ = Math.max(innerData[p].z, maxZ);
-                minZ = Math.min(innerData[p].z, minZ);
+                maxY = Math.max(innerData[p].y, maxY);
+                minY = Math.min(innerData[p].y, minY);
             }
 
             for(var i = 0; i < nbPoints - 1; i++) {
@@ -206,18 +185,19 @@ export default {
             var normals = [];
             var uvs =[];
 
-            /*if(standardUV) {
+            /* if(standardUV) {
                 for(var p = 0; p < positions.length; p += 3) {
                     uvs.push((positions[p] - minX)/(maxX - minX), (positions[p + 2] - minZ)/(maxZ - minZ));               
                 }
             }
-            else {*/
+            else { */
                 var flip = 0;
                 var p1 = 0;
                 var p2 = 0;
                 var p3 = 0;
                 var v0 = innerData[0];
                 var v1 = innerData[1].subtract(v0);
+
                 var v2 = outerData[0].subtract(v0);
                 var v3 = outerData[1].subtract(v0);
                 var axis = v1.clone();
@@ -240,7 +220,6 @@ export default {
                 uvs[2 * indices[4] + 1] = 0;
             
                 for(var i = 6; i < indices.length; i +=6) {
-                
                     flip = (flip + 1) % 2;
                     v0 = innerData[0];
                     v1 = innerData[1].subtract(v0);
